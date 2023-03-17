dc92e99a0156a927df5db711c437e844d495ca70
joshhoneypot <joshuaangugo@gmail.com>
2023-03-17 09:06:23 +0300

    control flows

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+
+function scuberGreetingForFeet(ride){
   // Write your code here!
+
+  
+  if(ride>2500) 
+    return 'No can do.'
+
+  if (ride<=400) {
+    return 'This one is on me!'
+  } else (ride>2000)
+    return 'I will gladly take your thirty bucks.'
+
+}
+  
+function ternaryCheckCity(city){
+  // Write your code here!
+
+    return city === "NYC" 
+    ? "Ok, sounds good." 
+    : "No go.";
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+  function switchOnCharmFromTip(Tip){
+    switch(Tip){
+       case 'generous':
+         return  "Thank you so much." 
+       case  'not as generous':
+         return "Thank you."
+       default:
+         return  "Bye."}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
 }
